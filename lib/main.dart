10c93251f695c19c71dc448e096cce6a005f7881
mdShakilHossainNsu20!8 <shakilnsu2018@gmail.com>
2019-09-01 22:47:00 +0600

    lec 22

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("My First App"),
+        ),
+        body: Column(children: <Widget>[
+          Text("The Question?"),
+          RaisedButton(child: Text("Answer 1!") , onPressed: null),
+          RaisedButton(child: Text("Answer 2") , onPressed: null),
+          RaisedButton(child: Text("Answer 3") , onPressed: null),
+        ],),
+      ),
+    );
+  }
+}
